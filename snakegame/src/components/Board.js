2b3b15f549038ce36bd4932846f455a698781d6c
joaopedro9 <joaopedro@flyskyler.com>
2022-12-31 17:32:05 +0100

    added moves, head, tail

--- snakegame/src/components/Board.js
+++ snakegame/src/components/Board.js
@@ -19,47 +19,97 @@ const StyledBoard = styled.div`
 
 const Board = (props) => {
     const [head, setHead] = useState([Math.floor(board.ROWS/2), Math.floor(board.COLS/2)])
+    const [tail, setTail] = useState(head)
+    const [food, setFood] = useState([10, 15])
+    const [game, setGame] = useState("lost")
     const [move, setMove] = useState('ArrowLeft')
     const blankMatrix = Array(board.ROWS).fill().map(() => Array(board.COLS).fill(0));
-    blankMatrix[2][0] = 1
+    const moves = {"ArrowLeft":1 , "ArrowRight":2, "ArrowUp":3, "ArrowDown":4}
     const [matrix, setMatrix] = useState(blankMatrix)
-    let items = null
-    if (matrix) {
-        items = matrix.map((row, rowindex) => row.map((item, colindex) => <Square key={rowindex*100+colindex} value={matrix[rowindex][colindex]}>{[rowindex, colindex]}</Square>))
-    }
     useEffect(()=> {
-            if (true) {
+            if (game==="start") {
+                startGame()
+            }
+            if (game==="game") {
                 const interval = setInterval(() => {
                 refreshBoard(head, matrix);
-                console.log("apos refreshBoard", matrix)
             }, 500)  
             
             
-            return () => clearInterval(interval);}}
+            return () => clearInterval(interval)}}, [game]
     )
+    function startGame() {
+        let startBoard = blankMatrix
+        startBoard[head[0]][head[1]]=1
+        setMatrix(startBoard)
+        generateFood()
+        setGame("game")
+    }
+    function generateFood() {
+        setFood([Math.floor(Math.random()*board.ROWS), Math.floor(Math.random()*board.COLS)])
+    }
+    function toggleGame() {
+        setGame(game==="lost"? "start": game==="game"? "pause": "game")
+    }
     function handleKeyBoard(event) {
         console.log(move)
         setMove(event.code)
     }
     function refreshBoard(head, matrix) {
-        const moves = {1: "ArrowLeft", 2:"ArrowRight", 3:"ArrowUp", 4:"ArrowDown"}
-        nextBoard(matrix, head)
+        nextBoard()
     }
-    function nextBoard(matrix, head) {
-        console.log(matrix, head)
-        const newArray = head
-        newArray[1] -=1
-        setHead(newArray)
+    function checkMoveTail() {
+        return !(head[0]===food[0] && head[1]===food[1])
+    }
+    function checkMoveHead() {
+        console.log(checkCollision(nextStep(move, head)))
+        return checkCollision(nextStep(move, head))
+    }
+    function checkCollision(){
+        return (matrix[head[0]][head[1]] !==0 || !(0<=head[0]<=board.ROWS) ||  !(0<=head[1]<=board.COLS))
+    }
+    function nextStep(move, entity) {
+        switch(move) {
+            case "ArrowLeft":
+                return [entity[0], entity[1]-1]
+            case "ArrowRight":
+                return [entity[0], entity[1]+1]
+            case "ArrowUp":
+                return [entity[0]-1, entity[1]]
+            case "ArrowDown":
+                return [entity[0]+1, entity[1]]
+            default:
+                break
+        }
+    }
+    function update(move, entity, setEntity) {
+        setEntity(nextStep(entity))
+    } 
+    function nextBoard() {
         const newMatrix = matrix
-        newMatrix[head[0]][head[1]-1] = 1
-        console.log(newMatrix)
-        setMatrix(newMatrix)
+        console.log("tail", tail)
+        if (checkMoveTail) {
+            newMatrix[tail[0]][tail[1]] = 0
+            console.log(matrix, matrix[tail[0]][tail[1]],Object.keys(moves).find(key => moves[key] === matrix[tail[0]][tail[1]]))
+            update(Object.keys(moves).find(key => moves[key] === matrix[tail[0]][tail[1]]), tail, setTail)
+            console.log("tail apos", tail)
+        }
+        if (checkMoveHead()) {
+            setGame("lost")
+            return
+        }
+        update(move, head, setHead)
+        newMatrix[head[0]][head[1]] = moves[move]
+        setMatrix([...newMatrix])
     }
-    return (
+    return (<>
             <StyledBoard tabIndex={-1} onKeyDown={(e) => handleKeyBoard(e)} width={board.WIDTH} height={board.HEIGHT} rows={board.ROWS} cols={board.COLS} >
-                {items}
-            </StyledBoard >
-        
+                        { matrix.map((row, rowindex) => row.map((item, colindex) => <Square key={rowindex*100+colindex} isFood={rowindex===food[0] && colindex===food[1]} value={matrix[rowindex][colindex]}>{[rowindex, colindex]}</Square>))}
+                    </StyledBoard >
+            <button onClick={() => toggleGame()}> Game </button>
+    </>
+            
+
     )
 }
 
--- snakegame/src/components/Square.js
+++ snakegame/src/components/Square.js
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
 const StyledSquare = styled.div`
 width: ${props => props.width}px;
 height: ${props => props.height}px;
-background: ${props => props.value ? "black": "blue"};
+background: ${props => props.isFood? "yellow": props.value ? "black": "blue"};
 `;
 
 const Square = (props) => {
-    console.log(props.value)
-    
-    return (<StyledSquare value={props.value}>{props.value ? "white": "blue"}</StyledSquare>)
+    useEffect(()=> {
+        console.log("apos rasdfasdf")
+        
+        
+        return }, [props.value]
+) 
+    return (<StyledSquare value={props.value} isFood={props.isFood}> {props.isFood? "XXX": props.value ? "black": "blue"}</StyledSquare>)
 }
 export default Square
